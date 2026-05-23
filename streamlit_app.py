3baa3de8e76330bf34e99b48a150a22f9c494991
Negi-031 <bhawna.negi@spglobal.com>
2026-05-23 15:46:39 +0530

    Update streamlit_app.py
    added code to call API

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,6 @@ if ingredients_list:
             
     #st.write(ingredients_string)
 
-    
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
                     values ('""" + ingredients_string + """', '"""+name_on_order+ """')"""
     
@@ -44,5 +43,10 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         
         st.success('Your Smoothie is ordered!' + name_on_order, icon="✅")
+      
+# New section to display smoothiefroot nutrition information
+import requests  
+smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+st.text(smoothiefroot_response)
 
         
